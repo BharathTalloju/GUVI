9bf187f9fad064c3a31eb651711a3dee853078ed
BharathTalloju <BharathTalloju@localhost.localdomain>
2016-09-01 19:43:56 +0530

    non repeating

---
+++ unique.py
@@ -0,0 +1,11 @@
+try:
+    arr = map(int, raw_input().strip().split())
+except (TypeError, ValueError):
+    print "Invalid"
+    exit(1)
+    pass
+r = 0
+for i in arr:
+    r ^= i
+    pass
+print r
