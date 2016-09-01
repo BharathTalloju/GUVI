dbef9bf2bc6ce9b6d1afece26d67c0efa8e37d00
BharathTalloju <BharathTalloju@localhost.localdomain>
2016-09-01 19:09:04 +0530

    repeated numbers

---
+++ repeated_nums.py
@@ -0,0 +1,26 @@
+try:
+    arr = map(int,raw_input().strip().split())
+except (TypeError, ValueError):
+    print "Invalid Inputs"
+    exit(1)
+arr.sort()
+n = len(arr)
+if n == 1:
+    print arr[0]
+    pass
+else:
+    result = ''
+    i,j = 0,1
+    while j<n:
+        if arr[i] == arr[j]:
+            result += str(arr[i])+' '
+            while  j < n and arr[i] == arr[j]:
+                j += 1
+                pass
+            i = j
+            j += 1
+            pass
+        else:
+            i += 1
+            j += 1
+    print result
